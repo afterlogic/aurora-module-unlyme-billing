fd6474de0cea897f736271290bcc6a3efabffcf4
Vasil Sokolov <vasil.sokolov@gmail.com>
2025-01-31 14:41:08 +0200

    Hid unlyme settings for tenant admin

--- vue/manager.js
+++ vue/manager.js
@@ -1,4 +1,5 @@
 import AdminSettingsPerTenant from './components/AdminSettingsPerTenant'
+import store from 'src/store'
 
 export default {
   moduleName: 'UnlymeBilling',
@@ -18,17 +19,22 @@ export default {
   },
 
   getAdminTenantTabs () {
-    return [
-      {
-        tabName: 'billing-unlyme',
-        tabTitle: 'UNLYMEBILLING.ADMIN_SETTINGS_TAB_LABEL',
-        tabRouteChildren: [
-          { path: 'id/:id/billing-unlyme', component: AdminSettingsPerTenant },
-          { path: 'search/:search/id/:id/billing-unlyme', component: AdminSettingsPerTenant },
-          { path: 'page/:page/id/:id/billing-unlyme', component: AdminSettingsPerTenant },
-          { path: 'search/:search/page/:page/id/:id/billing-unlyme', component: AdminSettingsPerTenant },
-        ],
-      }
-    ]
+    const isUserSuperAdmin = store.getters['user/isUserSuperAdmin']
+    if (isUserSuperAdmin) {
+      return [
+        {
+          tabName: 'billing-unlyme',
+          tabTitle: 'UNLYMEBILLING.ADMIN_SETTINGS_TAB_LABEL',
+          tabRouteChildren: [
+            { path: 'id/:id/billing-unlyme', component: AdminSettingsPerTenant },
+            { path: 'search/:search/id/:id/billing-unlyme', component: AdminSettingsPerTenant },
+            { path: 'page/:page/id/:id/billing-unlyme', component: AdminSettingsPerTenant },
+            { path: 'search/:search/page/:page/id/:id/billing-unlyme', component: AdminSettingsPerTenant },
+          ],
+        }
+      ]
+    } else {
+      return []
+    }
   },
 }
