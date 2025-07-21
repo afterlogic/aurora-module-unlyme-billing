874647b8c7dc10caf17fbbdac3f881d85734e78c
Vasil Sokolov <vasil.sokolov@gmail.com>
2025-07-21 23:43:04 +0200

    Made the UpgradeNow button redirect to the billing screen

--- js/views/HeaderItemView.js
+++ js/views/HeaderItemView.js
@@ -4,18 +4,24 @@ var
 	_ = require('underscore'),
 
 	Settings = require('modules/%ModuleName%/js/Settings.js'),
-	CAbstractHeaderItemView = require('%PathToCoreWebclientModule%/js/views/CHeaderItemView.js')
+	CAbstractHeaderItemView = require('%PathToCoreWebclientModule%/js/views/CHeaderItemView.js'),
+	SettingsModule = require('modules/SettingsWebclient/js/Settings.js')
 ;
 
 function CHeaderItemView()
 {
-	CAbstractHeaderItemView.call(this);
+	CAbstractHeaderItemView.call(this)
 
-	this.sLink = Settings.PaymentLink;
+	this.sLink = Settings.PaymentLink
 }
 
-_.extendOwn(CHeaderItemView.prototype, CAbstractHeaderItemView.prototype);
+_.extendOwn(CHeaderItemView.prototype, CAbstractHeaderItemView.prototype)
 
-CHeaderItemView.prototype.ViewTemplate = '%ModuleName%_HeaderItemView';
+CHeaderItemView.prototype.ViewTemplate = '%ModuleName%_HeaderItemView'
 
-module.exports = new CHeaderItemView();
+CHeaderItemView.prototype.click = function ()
+{
+	window.location.hash = SettingsModule.HashModuleName + '/' + Settings.HashModuleName
+}
+
+module.exports = new CHeaderItemView()
